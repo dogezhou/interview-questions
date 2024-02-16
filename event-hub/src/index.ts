ae21977d215d430cebd9c057d464064f4fa90492
dogezhou <doge.zhou@outlook.com>
2024-02-16 22:56:45 +0800

    feat: finish eventhub

--- event-hub/src/index.ts
+++ event-hub/src/index.ts
@@ -1,9 +1,35 @@
+export type EventFn = (data: unknown) => void;
+
 class EventHub {
-    on() {
-        console.log('on')
+  private cache: { [k: string]: Array<EventFn> } = {};
+  on(eventName: string, fn: EventFn) {
+    if (this.cache[eventName]) {
+      this.cache[eventName].push(fn);
+    } else {
+      this.cache[eventName] = [fn];
     }
-    emit() {
-        console.log('emit')
+  }
+  emit(eventName: string, data?: unknown) {
+    if (this.cache[eventName]) {
+      this.cache[eventName].forEach((fn) => {
+        fn(data);
+      });
     }
+  }
+  off(eventName: string, fn: EventFn) {
+    if (this.cache[eventName]) {
+      this.cache[eventName] = this.cache[eventName].filter(
+        (item) => item !== fn
+      );
+    }
+  }
+  once(eventName: string, fn: EventFn) {
+    const onceFn: EventFn = (data) => {
+      fn(data);
+      this.off(eventName, onceFn);
+    };
+    this.on(eventName, onceFn);
+  }
 }
-export default EventHub
+
+export default EventHub;
--- event-hub/test/index.ts
+++ event-hub/test/index.ts
@@ -1,10 +1,49 @@
-import EventHub from "../src";
+import EventHub, { EventFn } from "../src";
 
-const eventHub = new EventHub();
+type TestCase = (message: string) => void;
 
-eventHub.on('test', (data) => {
-  console.log(data)
-})
-eventHub.emit('test', (data) => {
-  console.log(data)
-})
+const test1: TestCase = (message) => {
+  const eventHub = new EventHub();
+  const emmitData = "emmitData";
+  let called = false;
+
+  eventHub.on("testEvent", (data) => {
+    called = true;
+    console.assert(data === emmitData);
+  });
+  eventHub.emit("testEvent", emmitData);
+  console.assert(called);
+  console.log(message);
+};
+
+const test2: TestCase = (message) => {
+  const eventHub = new EventHub();
+  let called = false;
+  const fn: EventFn = () => {
+    called = true;
+  };
+  eventHub.on("testEvent", fn);
+  eventHub.off("testEvent", fn);
+  eventHub.emit("testEvent");
+  console.assert(called === false);
+  console.log(message);
+};
+
+const test3: TestCase = (message) => {
+  const eventHub = new EventHub();
+  const emmitData = "emmitData";
+  let calledCount = 0;
+  const fn: EventFn = (data) => {
+    calledCount += 1;
+    console.assert(data === emmitData);
+  };
+  eventHub.once("testEvent", fn);
+  eventHub.emit("testEvent", emmitData);
+  eventHub.emit("testEvent", emmitData);
+  console.assert(calledCount === 1);
+  console.log(message);
+};
+
+test1(".on 之后 .emit 会调用 .on 的函数");
+test2(".off 之后 .emit 不会调用 .on 的函数");
+test3(".once 之后 .emit只调用一次");
