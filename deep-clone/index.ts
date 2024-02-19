28870f83664a7c2cbc99c90147e190cc9b7cdd2e
dogezhou <doge.zhou@outlook.com>
2024-02-19 22:49:38 +0800

    feat: finish deep clone

--- deep-clone/index.ts
+++ deep-clone/index.ts
@@ -15,13 +15,13 @@ class DeepClone {
         if (!(source instanceof Object)) {
             return source
         }
-    
+
         // object with cache return cached target
         const cachedTarget = this.findCache(source)
         if (cachedTarget) {
             return cachedTarget
         }
-    
+
         // handle object cloning
         let target: T
         if (source instanceof Array) {
@@ -30,7 +30,7 @@ class DeepClone {
             // loadsh 是不克隆函数的，将返回 {}。 因为克隆函数没有实际意义，公用同一个函数也没问题。
             // 而且涉及到 科里化，this，闭包等问题，无法克隆相等价值的函数。​
             target = eval(source.toString()) // 安全问题
-    
+
             // target = new Function("return " + source.toString())()
             // target = function () {
             //     return source.apply(this, arguments)
@@ -42,10 +42,10 @@ class DeepClone {
         } else {
             target = new Object() as T
         }
-    
+
         // cache before calling deepClone
         this.cache.push([source, target])
-    
+
         for (let key in source) {
             if (source.hasOwnProperty(key)) {
                 target[key] = this.clone(source[key])
@@ -53,6 +53,33 @@ class DeepClone {
         }
         return target
     }
+
+    // clone<T>(source: T): T {
+
+    //     if (!(source instanceof Object)) {
+    //         return source
+    //     }
+
+    //     let result = {}
+    //     const stack: { source: object; target: object }[] = [
+    //         { source: source as object, target: result },
+    //     ]
+
+    //     while (stack.length) {
+    //         const { source, target } = stack.pop()
+    //         for (let key in source) {
+    //             if (source.hasOwnProperty(key)) {
+    //                 if (source[key] instanceof Object) {
+    //                     target[key] = {}
+    //                     stack.push({ source: source[key], target: target[key] })
+    //                 } else {
+    //                     target[key] = source[key]
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return result as T
+    // }
 }
 
 export default DeepClone
